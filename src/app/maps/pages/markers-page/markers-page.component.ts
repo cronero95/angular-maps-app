398b4ef75dd66d3978818fae6bd1c3802faf8ab0
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-29 18:06:40 -0500

    Add a marker in a map

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,55 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import mapboxgl, { LngLat, Marker } from 'mapbox-gl';
 
 @Component({
   templateUrl: './markers-page.component.html',
-  styles: ``
+  styleUrl: './markers-page.component.css'
 })
-export class MarkersPageComponent {
+export class MarkersPageComponent implements AfterViewInit {
+
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  public currentZoom: number = 12;
+  public map?: mapboxgl.Map;
+  public currentLngLat: LngLat = new LngLat(-76.5225, 3.43722);
+
+  ngAfterViewInit(): void {
+    if(!this.divMap) throw Error('HTML element not found.')
+
+    this.map = new mapboxgl.Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: this.currentZoom, // starting zoom
+    });
+
+    this.mapListeners();
+
+    const marker = new Marker({
+      color: 'green'
+    })
+      .setLngLat(this.currentLngLat)
+      .addTo(this.map);
+
+  }
+
+  mapListeners(): void {
+    if(!this.map) throw Error('Uninitialized map');
+
+    this.map.on('zoom', (event) => {
+      this.currentZoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (event) => {
+      if(this.map!.getZoom() < 18) return;
+
+      this.map!.zoomTo(18);
+    })
+
+    this.map.on('move', (env) => {
+      this.currentLngLat = this.map!.getCenter();
+    })
+  }
 
 }
