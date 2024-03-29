dacb2e3cfaabaa10d88f5807d557c5ee84dd901e
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-28 21:36:30 -0500

    Show a map from 'mapbox' in 'full-screen-page'

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import mapboxgl from 'mapbox-gl';
+
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoiY3JvbmVybzk1IiwiYSI6ImNsdWJxNjBtODB5YWcyams0dm84ZDljbnAifQ.5G3F6kGdxgvhQBZGFdgZXA';
+
 
 @Component({
   templateUrl: './full-screen-page.component.html',
-  styles: ``
+  styleUrl: './full-screen-page.component.css'
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+
+  }
 
 }
