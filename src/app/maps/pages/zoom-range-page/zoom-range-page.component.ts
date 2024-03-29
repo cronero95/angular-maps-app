407fc15ba2ed8e2e7376356e8cbc8c542116839f
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-29 17:25:17 -0500

    Add functionalties to the zoom-range component

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -10,16 +10,48 @@ export class ZoomRangePageComponent implements AfterViewInit {
   @ViewChild('map')
   public divMap?: ElementRef;
 
+  public currentZoom: number = 10;
+  public map?: mapboxgl.Map;
+
   ngAfterViewInit(): void {
     if(!this.divMap) throw Error('HTML element not found.')
 
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: this.divMap?.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.currentZoom, // starting zoom
     });
 
+    this.mapListeners();
+
+  }
+
+  mapListeners(): void {
+    if(!this.map) throw Error('Uninitialized map');
+
+    this.map.on('zoom', (event) => {
+      this.currentZoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (event) => {
+      if(this.map!.getZoom() < 18) return;
+
+      this.map!.zoomTo(18);
+    })
+  }
+
+  zoomIn(): void {
+    this.map?.zoomIn();
+  }
+
+  zoomOut(): void {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged(value: string): void {
+    this.currentZoom = Number(value);
+    this.map?.zoomTo(this.currentZoom);
   }
 
 }
