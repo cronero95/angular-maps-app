2719bccc50a2adcd8c311a5e39badda1ec9e420d
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-29 16:57:21 -0500

    Add structure to use zoom controls

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
-  styles: ``
+  styleUrl: './zoom-range-page.component.css'
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit {
+
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+    if(!this.divMap) throw Error('HTML element not found.')
+
+    const map = new mapboxgl.Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+
+  }
 
 }
