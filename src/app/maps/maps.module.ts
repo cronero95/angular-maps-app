76095abee6049cc9b65ed57e7d9c3be214281b60
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-30 13:19:53 -0500

    Use a standalone component in a normal component

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -15,6 +15,8 @@ import { MarkersPageComponent } from './pages/markers-page/markers-page.componen
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
 
+import { CounterAloneComponent } from '../alone/components/counter-alone/counter-alone.component';
+
 
 @NgModule({
   declarations: [
@@ -28,7 +30,8 @@ import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.
   ],
   imports: [
     CommonModule,
-    MapsRoutingModule
+    MapsRoutingModule,
+    CounterAloneComponent
   ]
 })
 export class MapsModule { }
