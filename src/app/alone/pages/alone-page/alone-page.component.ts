4b92bea22cb9c5588765e08344b10408719e3552
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-30 12:55:34 -0500

    Create standalone component and use in it lazyload
    Create 'alone-page' component to be a standalone main page in app.
    Create 'counter-alone' component to be added in the 'alone-page' component.

---
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [
+
+  ],
+  templateUrl: './alone-page.component.html',
+  styleUrl: './alone-page.component.css'
+})
+export class AlonePageComponent {
+
+}
